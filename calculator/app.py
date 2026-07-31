4f5165d1262ebdc567c1fb92c8e5b02d7c1a11e9
BangsGlobal7 <abdoul.bangoura@global-itec.com>
2026-07-31 18:19:06 +0000

    feature: ajout du menu et l'import dans app

---
+++ calculator/app.py
@@ -0,0 +1,4 @@
+# import de la fonction affichage
+import menu
+
+menu.afficher_menu()
---
+++ calculator/menu.py
@@ -0,0 +1,20 @@
+# Fonction pour afficher le menu principal
+
+def afficher_menu():
+
+    print("=======================================================================================================")
+    print("CALCULATRICE PROFESSIONNELLE")
+    print("=======================================================================================================")
+
+    # Les options sont regroupées ici afin de garder une présentation cohérente
+    # si le menu évolue dans les prochaines versions.
+    
+    print("1. Addition")
+    print("2. Soustraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("0. Quitter")
+
+    print("=======================================================================================================")
+    print("CALCULATRICE PROFESSIONNELLE")
+    print("=======================================================================================================")
