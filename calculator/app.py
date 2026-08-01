260374d8072b28ef2c147b7c5400b0a0c133700e
BangsGlobal7 <abdoul.bangoura@global-itec.com>
2026-08-01 17:14:59 +0000

    fix: correction dde la bouche. A present inclut dans la function lire le choix

--- calculator/app.py
+++ calculator/app.py
@@ -1,10 +1,12 @@
+# import
 import menu
 import validator
 
-
+# initialisation 
+CHOIX_VALIDES = [0, 1, 2, 3, 4]
 choix = -1
 
 while choix != 0:
     menu.afficher_menu()
-    choix = validator.lire_choix()
+    choix = validator.lire_choix(CHOIX_VALIDES)
     print(choix)
--- calculator/validator.py
+++ calculator/validator.py
@@ -1,4 +1,13 @@
 
-def lire_choix():
+def lire_choix(choix_valides):
+
     choix = int(input("Votre choix : "))
+
+    while choix not in choix_valides:
+        print("Choix invalide")
+        choix = int(input("Nouveau choix  : "))
+
     return choix
+
+
+
